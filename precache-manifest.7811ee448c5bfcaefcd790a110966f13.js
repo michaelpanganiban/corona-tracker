5a1088473fccbdd78e51c73933db045b5c0f1733
John Michael Panganiban <johnmichaelpanganiban.its@gmail.com>
2020-06-12 18:19:01 +0800

    Updates

---
+++ precache-manifest.7811ee448c5bfcaefcd790a110966f13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70a6b8895eced52587f02b504c352ad0",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "cd7affd33801f42a19b7",
+    "url": "/corona-tracker/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cd7affd33801f42a19b7",
+    "url": "/corona-tracker/static/js/2.e5200dcc.chunk.js"
+  },
+  {
+    "revision": "ebec6ec5880fcec562f9a7747ae47b51",
+    "url": "/corona-tracker/static/js/2.e5200dcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27bfa021d8b1b9be1998",
+    "url": "/corona-tracker/static/js/main.0e000e9c.chunk.js"
+  },
+  {
+    "revision": "c80e65afd8aad5d56bf2",
+    "url": "/corona-tracker/static/js/runtime-main.fbff3bb5.js"
+  }
+]);
